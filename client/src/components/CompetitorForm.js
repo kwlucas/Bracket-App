f23e6180d558a27fa713ef71be84cebed2b2ac05
kwlucas <lucaswkyle@gmail.com>
2023-02-01 13:42:03 -0700

    Basic structure

--- client/src/components/CompetitorForm.js
+++ client/src/components/CompetitorForm.js
@@ -1,7 +1,13 @@
+import React from "react";
+
 function CompetitorForm(props) {
   
     return (
-      <></>
+      <form>
+        <label>Name: </label>
+        <input></input>
+        <button type="submit"></button>
+      </form>
     );
   }
   
